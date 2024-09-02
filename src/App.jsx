fbac346187beacb23b0f74d6d2d084bbe91016dd
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-02 20:20:50 +0545

    create context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TestimonialSlider from "./TestimonialSlider"; 
 import ThemeButton from "./ThemeButton";
+import { ThemeProvider } from "./context/Theme";
 
 function App() {
+
+  const [themeMode, setThemeMode] = useState("light");
+  const lightMode=()=>{
+    setThemeMode("light")
+  }
+  const darkMode=()=>{
+    setThemeMode("dark")
+  }
+
+useEffect(() => {
+  const switcher =document.querySelector('html').classList;
+  switcher.remove("light","dark");
+  switcher.add(themeMode)
+}, [themeMode])
+
+
+
   return (
+    <ThemeProvider values={{themeMode,darkMode,lightMode}}>
     <div className="bg-gradient-to-b from-[#c47b79ce] to-[#864af5b7] w-full h-full overflow-y-auto pb-20 dark:bg-gradient-to-b dark:from-[#4e4c4c] dark:to-[#000000b7]">
       <ThemeButton/>
       <TestimonialSlider />
     </div>
+    </ThemeProvider>
   );
 }
 
